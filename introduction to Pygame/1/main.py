affbd5be69f32999561edec37a91f5cf48ac1b98
Your Name <paologugliel@gmail.com>
2025-03-03 17:17:56 +0100

    Version #5

--- introduction to Pygame/1/main.py
+++ introduction to Pygame/1/main.py
@@ -10,33 +10,43 @@ pygame.display.set_caption(CAPTION)
 clock = pygame.time.Clock()
 text_font = pygame.font.Font('../font/Pixeltype.ttf', 70)
 
-# Create surfaces
+# Create environment surfaces
 sky_surface = pygame.image.load("../graphics/Sky.png").convert()
 ground_surface = pygame.image.load("../graphics/ground.png").convert()
-text_surface = text_font.render('My game', True, 'Black')
 
+# Create score surface and rectangle
+score_surface = text_font.render('My game', True, 'Black')
+score_rect = score_surface.get_rect(center=(400, 50))
+
+# Create snail surface and rectangle
 snail_surface = pygame.image.load('../graphics/snail/snail1.png').convert_alpha()
 snail_rectangle = snail_surface.get_rect(topleft=SNAIL_POSITION)
 
+# Create player surface and rectangle
 player_surface = pygame.image.load('../graphics/Player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(topleft=(80, 215))
 
 # Main loop
 while True:
 
-    # Check for quit input
+    # Check for inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit_game()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
 
     # Position the surfaces on the display surface
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    screen.blit(score_surface, score_rect)
+
     screen.blit(player_surface, player_rectangle)
     screen.blit(snail_surface, snail_rectangle)
 
-    # Move snail
+    # Move the snail
     snail_rectangle.x -= SNAIL_SPEED
     # Check for out of bounds
     if snail_rectangle.right <= 0:
@@ -44,7 +54,13 @@ while True:
 
     # Check for collision
     if player_rectangle.colliderect(snail_rectangle):
+        print("Snail collision! Game aborted")
         quit_game()
+
+    # Check for mouse collision with the player
+    mouse_position = pygame.mouse.get_pos()
+    if player_rectangle.collidepoint(mouse_position):
+        print("Mouse collision!")
 
     # Update everything
     pygame.display.update()
