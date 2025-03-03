b9f168b40844f55220a5aef77ef15c6dec7cf408
Your Name <you@example.com>
2025-03-03 09:52:56 +0100

    Version #3

--- introduction to Pygame/1/main.py
+++ introduction to Pygame/1/main.py
@@ -2,15 +2,20 @@ import pygame
 import sys
 from settings import *
 
+# Initialize
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(CAPTION)
 clock = pygame.time.Clock()
+text_font = pygame.font.Font('../font/Pixeltype.ttf', 70)
 
 # Create surfaces
-sky_surface = pygame.image.load("../graphics/Sky.png")
-ground_surface = pygame.image.load("../graphics/ground.png")
+sky_surface = pygame.image.load("../graphics/Sky.png").convert()
+ground_surface = pygame.image.load("../graphics/ground.png").convert()
+text_surface = text_font.render('My game', True, 'Black')
+snail_surface = pygame.image.load('../graphics/snail/snail1.png').convert_alpha()
 
+# Main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,10 +23,18 @@ while True:
             sys.exit()
 
     # Position the surfaces on the display surface
-    screen.blit(sky_surface, (200, 100))
-    screen.blit(ground_surface, (500, 300))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (SNAIL_X_POS, 265))
 
-    # Draw all my elements
+    # Move snail
+    SNAIL_X_POS -= 4
+
+    # Check for EOS (End Of Screen)
+    if SNAIL_X_POS <= -100:
+        SNAIL_X_POS = 810
+
     # Update everything
     pygame.display.update()
     clock.tick(FRAME_RATE)
--- introduction to Pygame/1/settings.py
+++ introduction to Pygame/1/settings.py
@@ -1,8 +1,11 @@
 # Display
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 400
 CAPTION = "Runner"
 
 # Clock
 FRAME_RATE = 60
+
+# Snail position
+SNAIL_X_POS = 700
 
