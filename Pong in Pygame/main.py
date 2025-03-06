0833694cd14f89788b7b7f1c74fdc97fef2b571a
Your Name <paologugliel@gmail.com>
2025-03-06 21:08:49 +0100

    Set up project and draw elements on screen.

--- Pong in Pygame/main.py
+++ Pong in Pygame/main.py
@@ -1,1 +1,42 @@
 import pygame
+import sys
+
+# General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting up the main window
+screen_width = 1280
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong")
+
+# Game rectangles
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
+
+# Colors
+background_color = pygame.Color('grey12')
+light_grey = (200, 200, 200)
+
+while True:
+    # Handling input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Draw elements
+    screen.fill(background_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    # draw the middle line
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
+
+    # Update the window
+    pygame.display.flip()
+    clock.tick(60)
+
+
