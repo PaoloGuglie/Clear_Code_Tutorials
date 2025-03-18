ff83110bc23a64d0be54d772507452904d8c0c29
Your Name <paologugliel@gmail.com>
2025-03-18 21:59:45 +0100

    Placed a proper floor using an image created with a map editor.

--- Zelda-like in Python/code/level.py
+++ Zelda-like in Python/code/level.py
@@ -49,10 +49,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_height() // 2
         self.offset = pygame.math.Vector2()  # offset for camera centering
 
+        # Floor
+        self.floor_surface = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
-        # get the offset coordinates
+        # get the player offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Draw the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             # get the offset position for each element
