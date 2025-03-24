6a0fb75ec9f6d4ef7969ec2e4d3f7033e3357234
Your Name <paologugliel@gmail.com>
2025-03-24 11:53:04 +0100

    Now the menus display the items names and costs.

--- Zelda-like in Python/code/upgrade_menu.py
+++ Zelda-like in Python/code/upgrade_menu.py
@@ -11,7 +11,14 @@ class UpgradeMenu:
         self.player = player
         self.attribute_number = len(player.stats)
         self.attribute_names = list(player.stats.keys())
+        self.max_values = list(player.max_stats.values())
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        # Items creation
+        self.height = self.display_surface.get_height() * 0.8
+        self.width = self.display_surface.get_width() // 6
+        self.item_list = None
+        self.create_items()
 
         # Selection system
         self.selection_index = 0
@@ -51,6 +58,65 @@ class UpgradeMenu:
             if current_time - self.selection_time >= self.selection_cooldown:
                 self.can_move = True
 
+    def create_items(self):
+        self.item_list = []
+
+        for i in range(self.attribute_number):
+
+            # Horizontal position
+            full_width = self.display_surface.get_width()
+            increment = full_width // self.attribute_number
+            left = (i * increment) + (increment - self.width) // 2
+
+            # Vertical position
+            top = self.display_surface.get_height() * 0.1
+
+            # Create the object
+            item = Item(left, top, self.width, self.height, i, self.font)
+            self.item_list.append(item)
+
     def display(self):
         self.input()
         self.cooldowns()
+
+        # Upgrade windows
+        for index, item in enumerate(self.item_list):
+            # attributes
+            name = self.attribute_names[index]
+            value = self.player.get_value_by_index(index)
+            max_value = self.max_values[index]
+            cost = self.player.get_cost_by_index(index)
+            # display
+            item.display(
+                self.display_surface,
+                self.selection_index,
+                name,
+                value,
+                max_value,
+                cost)
+
+
+class Item:
+    def __init__(self, left, top, width, height, index, font):
+        self.rect = pygame.Rect(left, top, width, height)
+        self.index = index
+        self.font = font
+
+    def display_names(self, surface, name, cost, selected):
+
+        # Title text
+        title_surface = self.font.render(name, False, TEXT_COLOR)
+        title_rect = title_surface.get_rect(midtop=self.rect.midtop + pygame.math.Vector2(0, 20))
+
+        # Cost text
+        cost_surface = self.font.render(f'{int(cost)}', False, TEXT_COLOR)
+        cost_rect = cost_surface.get_rect(midbottom=self.rect.midbottom - pygame.math.Vector2(0, 20))
+
+        # Draw
+        surface.blit(title_surface, title_rect)
+        surface.blit(cost_surface, cost_rect)
+
+    def display(self, surface, selection_num, name, value, max_value, cost):
+        pygame.draw.rect(surface, UI_BG_COLOR, self.rect)
+
+        self.display_names(surface, name, cost, False)
