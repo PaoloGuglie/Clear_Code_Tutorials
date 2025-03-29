181a5a55dc5d13072a482611323ebca7860a9d92
Your Name <paologugliel@gmail.com>
2025-03-29 11:47:40 +0100

    Decorators with arguments.

---
+++ Decorators in Python/decorators_with_arguments.py
@@ -0,0 +1,59 @@
+# I can have functions with parameters being decorators.
+
+# Decorators can also have parameters. Complicated because I would be wrapping
+# a function inside a function inside another function.
+
+def decorator(func):
+    def wrapper():
+        print("Decorator begins.")
+        func()
+        print("Decorator ends.")
+
+    return wrapper
+
+
+@decorator
+def func(func_parameter):
+    print(func_parameter)
+
+
+try:
+    func('hello')
+
+except TypeError:
+    print("Error")
+
+# I will now get an error because the function func() needs a parameter, but
+# in the decorator func() is called without arguments.
+print("------------------")
+
+
+# Correct code:
+
+def decorator(func):
+    """ Added wrapper parameter used as function argument """
+
+    def wrapper(wrapper_parameter):
+        print("Decorator begins.")
+        func(wrapper_parameter)
+        print("Decorator ends.")
+
+    return wrapper
+
+
+@decorator
+def func(func_parameter):
+    print(func_parameter)
+
+
+func('something...')
+
+
+# I can have this to have the decorator accept any kind of function with any
+# kind of parameter:
+def decorator(func):
+
+    def wrapper(*args, **kwargs):
+        print("decoration begins")
+        func(*args, **kwargs)
+        print("decoration ends")
--- Decorators in Python/function_twice.py
+++ Decorators in Python/function_twice.py
@@ -22,7 +22,11 @@ def other_decorator(func):
 @other_decorator
 def func():
     """ First, the other_decorator() function is applied to func(),
-    then the resulting code is run twice because of double_decorator() """
+    then the resulting code is run twice because of double_decorator().
+    Each func() call of double_decorator() is referring to the wrapper()
+    function of other_decorator(), which in turn has each func() referring
+    to func() itself. """
+
     print("Function")
 
 
