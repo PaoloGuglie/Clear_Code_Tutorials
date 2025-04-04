2889be88ad410125733fd7e334029111c20c4173
Your Name <paologugliel@gmail.com>
2025-04-04 13:46:42 +0200

    pass and input.

---
+++ Guide to Python/pass_and_input.py
@@ -0,0 +1,12 @@
+# "pass" tells python to do nothing. Only used with a function but no content
+
+
+def func():
+    pass
+
+
+# Input is one way to get user input
+
+user_input = input("  - ")
+
+print(f"Your input is: {user_input}")
