9837e492f0eebd2637b06262e4a3a26a36d44c25
Your Name <paologugliel@gmail.com>
2025-04-10 18:47:13 +0200

    Create sliding sidebar with animation.

--- GUI in Python/animated_widget.py
+++ GUI in Python/animated_widget.py
@@ -2,6 +2,54 @@ import customtkinter as ctk
 from random import choice
 
 
+class SlidePanel(ctk.CTkFrame):
+    def __init__(self, parent, start_pos, end_pos):
+        super().__init__(master=parent, fg_color='red')
+
+        # General attributes
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.width = abs(start_pos - end_pos)
+
+        # Animation logic
+        self.pos = start_pos
+        self.in_start_pos = True
+
+        # Layout
+        self.place(relx=self.start_pos,
+                   rely=0,
+                   relwidth=self.width,
+                   relheight=1)
+
+    def animate(self):
+        if self.in_start_pos:
+            self.animate_forward()
+        else:
+            self.animate_backwards()
+
+    def animate_forward(self):
+        if self.pos > self.end_pos:
+            self.pos -= 0.008
+            self.place(relx=self.pos,
+                       rely=0,
+                       relwidth=self.width,
+                       relheight=1)
+            self.after(10, self.animate_forward)
+        else:
+            self.in_start_pos = False
+
+    def animate_backwards(self):
+        if self.pos < self.start_pos:
+            self.pos += 0.008
+            self.place(relx=self.pos,
+                       rely=0,
+                       relwidth=self.width,
+                       relheight=1)
+            self.after(10, self.animate_backwards)
+        else:
+            self.in_start_pos = True
+
+
 def move_button():
     global button_x
     button_x += 0.004
@@ -18,11 +66,14 @@ window = ctk.CTk()
 window.title('Animated Widgets')
 window.geometry('600x400')
 
+# Animated widged
+animated_panel = SlidePanel(window, 0, -0.3)
+
 # Sidebar toggle button
 button_x = 0.5
 sidebar_toggle_button = ctk.CTkButton(window,
                                       text='toggle sidebar',
-                                      command=move_button)
+                                      command=animated_panel.animate)
 
 sidebar_toggle_button.place(relx=button_x,
                             rely=0.5,
