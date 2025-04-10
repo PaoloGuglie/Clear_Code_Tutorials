846940393adea05105b508446e788681e90434b2
Your Name <paologugliel@gmail.com>
2025-04-10 14:57:35 +0200

    Continued working in animated widgets in tkinter.

--- GUI in Python/animated_widget.py
+++ GUI in Python/animated_widget.py
@@ -1,14 +1,32 @@
 import customtkinter as ctk
+from random import choice
+
+
+def move_button():
+    global button_x
+    button_x += 0.004
+    sidebar_toggle_button.place(relx=button_x,
+                                rely=0.5,
+                                anchor='center')
+
+    if button_x < 0.9:
+        window.after(10, move_button)
+
 
 # Window
 window = ctk.CTk()
 window.title('Animated Widgets')
 window.geometry('600x400')
 
-# Button
+# Sidebar toggle button
 button_x = 0.5
-button = ctk.CTkButton(window, text = 'toggle sidebar')
-button.place(relx=button_x, rely=0.5, anchor='center')
+sidebar_toggle_button = ctk.CTkButton(window,
+                                      text='toggle sidebar',
+                                      command=move_button)
+
+sidebar_toggle_button.place(relx=button_x,
+                            rely=0.5,
+                            anchor='center')
 
 
 def main() -> None:
